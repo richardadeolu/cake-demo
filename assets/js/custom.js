39cc9f2d006b5c028663a89fffda259420a58a53
richardadeolu <79562677+richardadeolu@users.noreply.github.com>
2023-12-18 11:29:26 +0100

    initial commit

---
+++ assets/js/custom.js
@@ -0,0 +1,13 @@
+        $(document).ready(function() {
+            $("#nav-button").click(function() {
+                $("#social-icons").toggle();
+            });
+            $("#nav-button").click(function() {
+                $("#social").toggle();
+            });
+        });
+
+
+        $(document).ready(function() {
+            $("#myModal").modal('show');
+        });
